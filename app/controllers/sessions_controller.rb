7871dd5d5839c1a6487ac1849edb50114c1c45a0
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-08-02 18:03:25 +0000

    user authentication successful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,25 @@
 class SessionsController < ApplicationController
+
   def create
-    user = User.find_by_email(params[:email])
+    @user = User.find_by('email': params[:email])
+    puts "params is... #{params}"
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
+      puts "user authenticated.."
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
+      puts " not logging in..."
       redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/login', error: "Wrong Password or Username"
   end
 
 end
